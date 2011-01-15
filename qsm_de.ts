cfe2830155dd5eb79f430d8ae8f0c04258db0fb0
Alexander Vos <vos@vossoft.de>
2011-01-15 19:30:51 +0100

    added german translation file

---
+++ qsm_de.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<defaultcodec>ISO-8859-15</defaultcodec>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="28"/>
+        <source>&amp;File</source>
+        <translation>&amp;Datei</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="35"/>
+        <source>Toolbar</source>
+        <translation>Toolbar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="47"/>
+        <source>Folder Browser</source>
+        <translation>Dateibrowser</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="68"/>
+        <source>Image List</source>
+        <translation>Bildliste</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="84"/>
+        <source>Image List Options</source>
+        <translation>Bildlistenoptionen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="96"/>
+        <source>Include Subfolders</source>
+        <translation>Unterordner einbeziehen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="111"/>
+        <source>Slideshow List</source>
+        <translation>Slideshow Liste</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="129"/>
+        <source>Slideshow Image List</source>
+        <translation>Slideshow Bildliste</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="145"/>
+        <source>Slideshow Options</source>
+        <translation>Slideshow Optionen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="157"/>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+</context>
+</TS>
