f71aeace4d1f534bb1133c6b1c1a9ef6825ff14f
Alexander Vos <vos@vossoft.de>
2011-01-16 22:26:21 +0100

    added multithreaded image loading for the thumbnails

--- imagelistmodel.cpp
+++ imagelistmodel.cpp
@@ -1,10 +1,34 @@
 #include "imagelistmodel.h"
 
+#include <QtCore>
+#include <QFutureWatcher>
 #include <QPixmap>
+#include <QPainter>
+
+QIcon ImageListModel::DEFAULT_ICON;
 
 ImageListModel::ImageListModel(QObject *parent) :
     QAbstractListModel(parent), m_fileCount(0)
 {
+    m_thumbnailWatcher = new QFutureWatcher<QImage>(this);
+    connect(m_thumbnailWatcher, SIGNAL(resultReadyAt(int)), SLOT(thumbnailWatcher_resultReadyAt(int)));
+    connect(m_thumbnailWatcher, SIGNAL(finished()), SLOT(thumbnailWatcher_finished()));
+
+    if (ImageListModel::DEFAULT_ICON.isNull()) {
+        QPixmap icon(64, 64);
+        QPainter painter(&icon);
+        painter.setPen(Qt::red);
+        painter.fillRect(icon.rect(), Qt::white);
+        painter.drawRect(icon.rect());
+        painter.drawLine(0,0, 64,64);
+        painter.drawLine(64,0, 0,64);
+        ImageListModel::DEFAULT_ICON = QIcon(icon);
+    }
+}
+
+ImageListModel::~ImageListModel()
+{
+    delete m_thumbnailWatcher;
 }
 
 int ImageListModel::rowCount(const QModelIndex &) const
@@ -34,8 +58,8 @@ void ImageListModel::addImageFileInfoList(const QFileInfoList &files)
 
     beginInsertRows(QModelIndex(), m_fileList.size(), m_fileList.size() + files.size());
     m_fileList.append(files);
-    foreach (const QFileInfo &fileInfo, files)
-        m_iconList.append(QIcon(QPixmap(fileInfo.filePath()).scaled(64, 64, Qt::KeepAspectRatio, Qt::FastTransformation)));
+    for (int i = 0; i < files.count(); ++i)
+        m_iconList.append(ImageListModel::DEFAULT_ICON);
     endInsertRows();
 }
 
@@ -58,7 +82,46 @@ void ImageListModel::fetchMore(const QModelIndex &parent)
     int remainder = m_fileList.size() - m_fileCount;
     int itemsToFetch = qMin(100, remainder);
 
+    qDebug("fetch more: from %d to %d", m_fileCount, m_fileCount + itemsToFetch - 1);
+
+    if (m_thumbnailWatcher->isRunning()) {
+        m_thumbnailWatcher->cancel();
+        m_thumbnailWatcher->waitForFinished();
+    }
+
+    m_thumbnailIndex = m_fileCount;
+
+    QFileInfoList::const_iterator begin = m_fileList.begin() + m_thumbnailIndex;
+    QFileInfoList::const_iterator end = begin + itemsToFetch;
+    if (end > m_fileList.end())
+        end = m_fileList.end();
+
+    // start multithreaded image loading
+    m_thumbnailWatcher->setFuture(QtConcurrent::mapped(begin, end, ImageListModel::createThumbnail));
+
     beginInsertRows(parent, m_fileCount, m_fileCount + itemsToFetch - 1);
     m_fileCount += itemsToFetch;
     endInsertRows();
+}
+
+QImage ImageListModel::createThumbnail(const QFileInfo &fileInfo)
+{
+    return QImage(fileInfo.filePath()).scaled(64, 64, Qt::KeepAspectRatio, Qt::FastTransformation);
+}
+
+void ImageListModel::thumbnailWatcher_resultReadyAt(int index)
+{
+    qDebug("thumbnailWatcher_resultReadyAt(%d)", index);
+
+    QImage image = m_thumbnailWatcher->resultAt(index);
+    index += m_thumbnailIndex;
+    m_iconList[index] = QIcon(QPixmap::fromImage(image));
+
+    QModelIndex modelIndex = this->index(index);
+    emit dataChanged(modelIndex, modelIndex);
+}
+
+void ImageListModel::thumbnailWatcher_finished()
+{
+    qDebug("thumbnailWatcher_finished");
 }
--- imagelistmodel.h
+++ imagelistmodel.h
@@ -3,7 +3,10 @@
 
 #include <QAbstractListModel>
 #include <QFileInfoList>
+#include <QImage>
 #include <QIcon>
+
+template <typename T> class QFutureWatcher;
 
 class ImageListModel : public QAbstractListModel
 {
@@ -11,6 +14,7 @@ class ImageListModel : public QAbstractListModel
 
 public:
     explicit ImageListModel(QObject *parent = 0);
+    ~ImageListModel();
 
     int rowCount(const QModelIndex &parent = QModelIndex()) const;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
@@ -19,14 +23,24 @@ public slots:
     void addImageFileInfoList(const QFileInfoList &files);
     void clear();
 
+private slots:
+    void thumbnailWatcher_resultReadyAt(int index);
+    void thumbnailWatcher_finished();
+
 protected:
     bool canFetchMore(const QModelIndex &parent) const;
     void fetchMore(const QModelIndex &parent);
 
 private:
+    static QIcon DEFAULT_ICON;
+
     QFileInfoList m_fileList;
     QList<QIcon> m_iconList;
     int m_fileCount;
+
+    QFutureWatcher<QImage> *m_thumbnailWatcher;
+    static QImage createThumbnail(const QFileInfo &fileInfo);
+    volatile int m_thumbnailIndex;
 
 };
 
