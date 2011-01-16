2976c5523e42fa2bfbcd7ec08d7d653f023ebaaf
Alexander Vos <vos@vossoft.de>
2011-01-16 11:39:57 +0100

    translator fixed

--- main.cpp
+++ main.cpp
@@ -9,12 +9,12 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    QTranslator translator;
     if (QLocale::system().language() == QLocale::German) {
         QTextCodec::setCodecForTr(QTextCodec::codecForName("ISO-8859-15"));
-        QTranslator translator;
         translator.load("qsm_de");
-        a.installTranslator(&translator);
     }
+    a.installTranslator(&translator);
 
     MainWindow w;
     w.show();
