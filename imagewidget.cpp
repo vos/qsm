bf758fe59c93830b193cc37cf9ba9e157b1e5d6f
Alexander Vos <vos@vossoft.de>
2011-01-18 14:06:52 +0100

    added ImageWidget

---
+++ imagewidget.cpp
@@ -0,0 +1,92 @@
+#include "imagewidget.h"
+
+ImageWidget::ImageWidget(QWidget *parent) :
+    QGLWidget(parent)
+{
+    setMinimumSize(64, 64);
+
+    m_imageWidth = 512;
+    m_imageHeight = 512;
+    m_imageAspectRatio = 1.0f;
+}
+
+void ImageWidget::initializeGL()
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_CULL_FACE);
+
+    GLuint textureIds[1];
+    glGenTextures(1, textureIds);
+
+    glBindTexture(GL_TEXTURE_2D, textureIds[0]);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+}
+
+void ImageWidget::resizeGL(int width, int height)
+{
+    glViewport(0, 0, width, height);
+    m_widgetAspectRatio = (float)width / (float)height;
+
+    updateProjection();
+}
+
+void ImageWidget::updateProjection()
+{
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    if (m_widgetAspectRatio < m_imageAspectRatio) {
+        int projection_height = int(m_imageWidth / m_widgetAspectRatio);
+        int top = (m_imageHeight - projection_height) / 2;
+        gluOrtho2D(0, m_imageWidth, projection_height + top, top);
+    } else if (m_widgetAspectRatio > m_imageAspectRatio) {
+        int projection_width = int(m_imageHeight * m_widgetAspectRatio);
+        int left = (m_imageWidth - projection_width) / 2;
+        gluOrtho2D(left, projection_width + left, m_imageHeight, 0);
+    } else
+        gluOrtho2D(0, m_imageWidth, m_imageWidth, 0);
+
+    glMatrixMode(GL_MODELVIEW);
+
+    updateGL();
+}
+
+void ImageWidget::paintGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_QUADS);
+    {
+        glVertex2s(0, m_imageHeight);
+        glTexCoord2f(1.0f, 1.0f);
+
+        glVertex2s(m_imageWidth, m_imageHeight);
+        glTexCoord2f(1.0f, 0.0f);
+
+        glVertex2s(m_imageWidth, 0);
+        glTexCoord2f(0.0f, 0.0f);
+
+        glVertex2s(0, 0);
+        glTexCoord2f(0.0f, 1.0f);
+    }
+    glEnd();
+
+    glFlush();
+}
+
+void ImageWidget::setImage(const QImage &image)
+{
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width(), image.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, image.constBits());
+
+    if (image.width() != m_imageWidth || image.height() != m_imageHeight) {
+        m_imageWidth = image.width();
+        m_imageHeight = image.height();
+        m_imageAspectRatio = (float)m_imageWidth / (float)m_imageHeight;
+        updateProjection();
+    } else
+        updateGL();
+}
---
+++ imagewidget.h
@@ -0,0 +1,29 @@
+#ifndef IMAGEWIDGET_H
+#define IMAGEWIDGET_H
+
+#include <QGLWidget>
+#include <QImage>
+
+class ImageWidget : public QGLWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ImageWidget(QWidget *parent = 0);
+
+    void setImage(const QImage &image);
+
+private:
+    float m_widgetAspectRatio;
+    int m_imageWidth;
+    int m_imageHeight;
+    float m_imageAspectRatio;
+
+    void initializeGL();
+    void resizeGL(int width, int height);
+    void updateProjection();
+    void paintGL();
+
+};
+
+#endif // IMAGEWIDGET_H
