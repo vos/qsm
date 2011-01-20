72328434d05e36c1fed72c047b94e1f86c0bd3d1
Alexander Vos <vos@vossoft.de>
2011-01-20 17:20:09 +0100

    QImage converted to open gl context

--- imagewidget.cpp
+++ imagewidget.cpp
@@ -40,18 +40,17 @@ void ImageWidget::updateProjection()
     glLoadIdentity();
 
     if (m_widgetAspectRatio < m_imageAspectRatio) {
-        int projection_height = int(m_imageWidth / m_widgetAspectRatio);
-        int top = (m_imageHeight - projection_height) >> 1;
-        gluOrtho2D(0, m_imageWidth, projection_height + top, top);
+        int projectionHeight = int(m_imageWidth / m_widgetAspectRatio);
+        int top = (m_imageHeight - projectionHeight) >> 1;
+        gluOrtho2D(0, m_imageWidth, projectionHeight + top, top);
     } else if (m_widgetAspectRatio > m_imageAspectRatio) {
-        int projection_width = int(m_imageHeight * m_widgetAspectRatio);
-        int left = (m_imageWidth - projection_width) >> 1;
-        gluOrtho2D(left, projection_width + left, m_imageHeight, 0);
+        int projectionWidth = int(m_imageHeight * m_widgetAspectRatio);
+        int left = (m_imageWidth - projectionWidth) >> 1;
+        gluOrtho2D(left, projectionWidth + left, m_imageHeight, 0);
     } else
         gluOrtho2D(0, m_imageWidth, m_imageWidth, 0);
 
     glMatrixMode(GL_MODELVIEW);
-
     updateGL();
 }
 
@@ -61,16 +60,16 @@ void ImageWidget::paintGL()
 
     glBegin(GL_QUADS);
     {
-        glVertex2s(0, m_imageHeight);
-        glTexCoord2f(1.0f, 1.0f);
-
-        glVertex2s(m_imageWidth, m_imageHeight);
-        glTexCoord2f(1.0f, 0.0f);
-
-        glVertex2s(m_imageWidth, 0);
+        glVertex2s(0, 0);
         glTexCoord2f(0.0f, 0.0f);
 
-        glVertex2s(0, 0);
+        glVertex2s(0, m_imageHeight);
+        glTexCoord2f(1.0f, 0.0f);
+
+        glVertex2s(m_imageWidth, m_imageHeight);
+        glTexCoord2f(1.0f, 1.0f);
+
+        glVertex2s(m_imageWidth, 0);
         glTexCoord2f(0.0f, 1.0f);
     }
     glEnd();
@@ -83,9 +82,8 @@ void ImageWidget::setImage(const QImage &image)
     if (image.isNull())
         return;
 
-    qDebug("image format = %d", image.format());
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width(), image.height(), 0, GL_BGRA, GL_UNSIGNED_BYTE, image.constBits());
+    QImage texture = QGLWidget::convertToGLFormat(image);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture.width(), texture.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, texture.constBits());
 
     if (image.width() != m_imageWidth || image.height() != m_imageHeight) {
         m_imageWidth = image.width();
